708768972b1ee3ebe5520188be577107ec2c2263
Bryan <bryanbrenes1@gmail.com>
2020-01-07 11:35:33 -0600

    definicion de puntos en mapa

--- laboratorios/mapas/js/app.js
+++ laboratorios/mapas/js/app.js
@@ -1,7 +1,64 @@
-var mymap = L.map('mapa').setView([51.505, -0.09], 13);
+
+
+var mymap = L.map('mapa').setView([9.8582215, -83.9211454], 16);
+// var mymap = L.map('mapa').fitWorld();
+// mymap.setZoom(15);
+// mymap.locate({ setView: true, maxZoom: 18 });
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   id: 'mapbox/streets-v11',
-  accessToken: 'your.mapbox.access.token'
+  accessToken: 'pk.eyJ1IjoiYnJ5YW5icmVuZXMyMDE5IiwiYSI6ImNrNTQyNjhmdDA5OTUzZHFoNWpubXo0NGMifQ.85NahMDmJOucBOEiHTPbmg'
 }).addTo(mymap);
+
+rutas = [
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        9.8567, -83.9240
+      ]
+    },
+    "properties": {
+      "name": "Yokohama"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        9.8585, -83.9162
+      ]
+    },
+    "properties": {
+      "name": "Basho"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        9.8617, -83.9172
+      ]
+    },
+    "properties": {
+      "name": "Sunken"
+    }
+  }
+]
+
+var listaRutas = [];
+
+rutas.forEach(ruta => {
+  var [x, y] = ruta.geometry.coordinates;
+  listaRutas.push([x, y]);
+  var nombre = ruta.properties.name;
+  var marker = L.marker([x, y]).bindPopup(`<b>${nombre}</b>`).addTo(mymap);
+});
+
+var polygon = L.polygon(listaRutas).addTo(mymap);
+
+console.log(rutas);
---
+++ laboratorios/mapas/rutas/ruta.js
@@ -0,0 +1,42 @@
+export default rutas = [
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        9.856471,
+        -83.9279081
+      ]
+    },
+    "properties": {
+      "name": "Yokohama"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        9.8583255,
+        -83.9195459
+      ]
+    },
+    "properties": {
+      "name": "Basho"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        9.8560767,
+        -83.9197048
+      ]
+    },
+    "properties": {
+      "name": "Sunken"
+    }
+  }
+]
+
